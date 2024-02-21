f677285ddfd4181ff36c0439b9a5a7c2c9ad6147
Kornthiwa Wirata <kornthiwa.wir@everapp.io>
2024-02-21 17:36:00 +0700

    no message

--- src/login/dto/create-login.dto.ts
+++ src/login/dto/create-login.dto.ts
@@ -1,16 +1,34 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Length, IsEnum } from 'class-validator';
+import { UserRole } from '../entities/login.entity';
 
 export class CreateLoginDto {
   @IsString()
   @IsNotEmpty()
+  readonly username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 20)
   readonly password: string;
 
   @IsString()
   @IsNotEmpty()
-  readonly username: string;
+  readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly lname: string;
+
+  @IsEnum(UserRole)
+  readonly role: UserRole;
 }
 
 export class LoginDto {
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
--- src/login/entities/login.entity.ts
+++ src/login/entities/login.entity.ts
@@ -1,6 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+export enum UserRole {
+  ADMIN = 'admin',
+  NURSE = 'nurse',
+  DOCTOR = 'doctor',
+}
+
 export type LoginDocument = Document & Login;
 
 @Schema({ timestamps: true })
@@ -11,8 +17,17 @@ export class Login {
   @Prop({ type: String, required: true })
   password: string;
 
-  @Prop({ type: Boolean, required: false })
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, required: true })
+  lname: string;
+
+  @Prop({ type: Boolean, required: false, default: true })
   rememberMe?: boolean;
+
+  @Prop({ type: String, required: true, enum: UserRole })
+  role: UserRole;
 }
 
 export const LoginSchema = SchemaFactory.createForClass(Login);
