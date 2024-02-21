3bf97b049c5e1c388e8d86414c5a01177d33514b
Kornthiwa Wirata <kornthiwa.wir@everapp.io>
2024-02-21 10:17:30 +0700

    no message

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { LoginModule } from './login/login.module';
+import { MongodbModule } from './mongodb/mongodb.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongodbModule,
+    LoginModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
